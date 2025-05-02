c588b1e6826d9e45d640ae20507375721da9c247
Luigi Selmi <luigiselmi@gmail.com>
2025-05-02 15:09:59 +0200

    minor change

--- scripts/move_files.py
+++ scripts/move_files.py
@@ -5,7 +5,8 @@ import pathlib
 '''
 This script implements some functions to copy a subset of TIFF 
 images and masks from the BigEarthNet folder to another folder.
-The script can be executed from the command line using the command
+The script can be executed using the command line from the root
+folder of the dl_remote_sensing project repository with the command
 
 >python move_files.py ../data/BigEarthNet-S2/images/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP
 
