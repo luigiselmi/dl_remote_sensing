220a199217a375eb8bf528d3a90009e8abbaaef9
Luigi Selmi <luigiselmi@gmail.com>
2025-05-09 18:32:25 +0200

    code refactoring

--- scripts/bigearthnetv2_data_preparation.py
+++ scripts/bigearthnetv2_data_preparation.py
@@ -1,20 +1,119 @@
+import numpy as np
 import os
 import sys
 import pathlib
+import rasterio
+import warnings
+warnings.filterwarnings('ignore')
 
 '''
-This script implements some functions to copy a subset of TIFF 
-images and masks from the BigEarthNet folder to another folder.
+This script implements some functions to create PNG RGB images 
+from TIFF images and masks of the BigEarthNet dataset.
 The script can be executed using the command line from the root
 folder of the dl_remote_sensing project repository with the command
 
->python move_files.py ../data/BigEarthNet-S2/images/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP
+>python scripts/bigearthnetv2_data_preparation.py data/BigEarthNet-S2
 
 '''
-path = pathlib.Path(sys.argv[1])
+## ---------------------------------------------- Start functions definition -----------------------------------------------
+def list_data_files(root_path):
+    '''
+    This function creates a list of images each containing
+    lists of patches with three RGB bands each or a mask.
+    '''
+    images_list = []
+    images_paths = [pathlib.Path(x) for x in root_path.iterdir() if x.is_dir()]
+    for image_path in images_paths:
+        # print(image_path.name)
+        patches_list = []
+        for patches_path in image_path.iterdir():
+            bands_list = []
+            for band_path in patches_path.iterdir():
+                band_type = band_path.name[-7:]
+                if (band_type == 'B02.tif' or band_type == 'B03.tif' or band_type == 'B04.tif' or band_type == 'map.tif'):
+                    bands_list.append(band_path)
+            patches_list.append(bands_list)
+        images_list.append(patches_list)
+    return images_list
 
-for file in os.listdir(path):
-    print(file)
+def print_raster_list(images_list): 
+    '''
+    Prints the content of the nested folders
+    within the list passed as argument.
+    images[patches[bands[]]]
+    '''
+    for patches_list in images_list:
+        for bands_list in patches_list:
+            for band in reversed(bands_list):
+                print(band.name)
+            print('\n')
+        print('\n')
+        
+def normalize(data_array):
+    return (data_array - np.min(data_array)) * ((255 - 0) / (np.max(data_array) - np.min(data_array))) + 0
 
-num_files = len(os.listdir(path))
-print('Number of files in {0:s}: {1:d}'.format(str(path), num_files))
+def createPNG(source_path_list, target_path):
+    '''
+    This function creates a multiband PNG file from a list of GeoTIFF files 
+    containing one band each. For an RGB file the source list shall contain three bands
+    in the RGB order. For Sentinel-2 it is B04, B03, B02. If the target file already exists
+    it doesn't create a new one and will return 1, otherwise it will create a new raster
+    and will return 0 
+    '''
+    SUCCESS = 0
+    FAILURE = 1
+    if (os.path.isfile(target_path)):
+        return FAILURE 
+        
+    band_list = []
+    dataset = rasterio.open(source_path_list[0])
+    width = dataset.width
+    height = dataset.height
+    count = len(source_path_list)
+
+    for raster_path in source_path_list:
+        dataset = rasterio.open(raster_path)
+        band = normalize(dataset.read(1))
+        band_list.append(band)
+        
+        
+    with rasterio.open(target_path,
+                    mode='w',
+                    driver='PNG',
+                    height=height,
+                    width=width,
+                    count=count,
+                    dtype='uint8') as target_dataset:
+        band_index = 1
+        for band in band_list:
+            target_dataset.write(band, band_index)
+            band_index += 1
+    return SUCCESS
+
+def createPNGs(images_list):
+    '''
+    This function creates a PNG for each patch of the images
+    in the list. The path of the PNG files is added to a list 
+    that will be returned. In case a PNG file already exist it 
+    only adds its path to the list.
+    '''
+    png_patches = []
+    for patches_list in images_list:
+        for i, bands_list in enumerate(patches_list): 
+            png_file_name = str(bands_list[i])[:-4] + '.png'
+            # print(i, png_file_name)
+            if (createPNG(bands_list, png_file_name) == 1):
+                print('The PNG file already exists.')
+                png_patches.append(png_file_name)
+            else:
+                png_patches.append(png_file_name)
+    return png_patches
+
+## ---------------------------------------------- Stop functions definition -----------------------------------------------
+
+#BIGEARTHNETv2_DIR = sys.argv[1]
+BIGEARTHNETv2_DIR = 'data/BigEarthNet-S2'
+print('Path to BigEarthNetv2 dataset: {:}'.format(BIGEARTHNETv2_DIR))
+
+IMAGES_DATA_DIR = pathlib.Path(BIGEARTHNETv2_DIR + '/images')
+MASKS_DATA_DIR = pathlib.Path(BIGEARTHNETv2_DIR + '/masks')
