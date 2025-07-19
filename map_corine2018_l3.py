e60a0d58247b0b92946d3cf463b87fa386cd122b
Luigi Selmi <luigiselmi@gmail.com>
2025-07-19 17:05:53 +0200

    changed file name

--- map_corine2018_l3.py
+++ map_corine2018_l3.py
@@ -1,12 +1,12 @@
 from lib.bigearthnetv2_lib import *
 '''
 This script can be used to  create new mask PNG files in a target folder 
-from source mask PNG files in the source folder to map the Corine2018 color 
-codes of the source files to their index in [1, 45]. The script can be 
-run using the command line from the root folder of the dl_remote_sensing 
-project repository with the command
+from source mask PNG files in the source folder to map the Corine2018 
+level 3 color codes of the source files to their index in [1, 45]. The 
+script can be run using the command line from the root folder of the 
+dl_remote_sensing project repository with the command
 
->python map_corine2018.py source_folder/ target_folder/
+>python map_corine2018_l3.py source_folder/ target_folder/
 
 Source and target folder paths must end with the forward slash.
 '''
@@ -17,7 +17,7 @@ TARGET_DIR = sys.argv[2]
 print('Source folder: ', SOURCE_DIR)
 print('Target folder: ', TARGET_DIR)
 
-nc_masks = mapCorine145_list(SOURCE_DIR, TARGET_DIR)
+nc_masks = mapCorineL3_list(SOURCE_DIR, TARGET_DIR)
 num_target_masks = len(nc_masks)
 
 print('Printing the unique values of the first 10 new masks:')
@@ -28,7 +28,7 @@ for mask in nc_masks[:10]:
 print('New {:d} mask files created in {}'.format(num_target_masks, TARGET_DIR))
 
 parent_folder = str(pathlib.Path(TARGET_DIR).parent)
-zip_file_name = parent_folder + '/' + 'nc_masks.zip'
+zip_file_name = parent_folder + '/' + 'l3_masks.zip'
 print('Zip file: ', zip_file_name)
  
 zip_pngs(nc_masks, zip_file_name)
