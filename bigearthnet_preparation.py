1bc660b1d622c2722486ece12866a2083ae070cb
Luigi Selmi <luigiselmi@gmail.com>
2025-07-20 21:35:11 +0200

    minor change

--- bigearthnet_preparation.py
+++ bigearthnet_preparation.py
@@ -4,7 +4,8 @@ from lib.bigearthnetv2_lib import *
 This script can be used to transform the TIFF files of the three RGB 
 bands of a patch into a PNG file with three bands. The script collects the 
 list of tiles and then processes the patches within each tile. The start tile
-and the end tile can be set in the script.
+and the end tile can be set in the script. The script creates the PNG files
+for the images and for the masks.
 The script can be executed using the command line from the root
 folder of the dl_remote_sensing project repository with the command
 
@@ -24,21 +25,31 @@ MASKS_DATA_DIR = pathlib.Path(BIGEARTHNETv2_DIR + '/Reference_Maps')
 start_tile_index = 0 
 end_tile_index = 5
 
+## Collect the tiles of the images
 tiles_list = list_image_files(IMAGES_DATA_DIR, start_tile_index, end_tile_index)
 num_rgb_bands = print_images_list(tiles_list)
 
+## collect the tiles of the masks
 tiles_mask_list = list_mask_files(MASKS_DATA_DIR, start_tile_index, end_tile_index)
 num_masks = print_masks_list(tiles_mask_list)
 
 num_tiles = len(tiles_list)
 
+## Creates the PNG files of the patches within the tiles. The PNG
+## images are saved in the same directory of the patch. The function
+## returns the list of the PNG files created
 pngs_list = createPNGs(tiles_list)
 
+## Creates the PNG files of the masks. The PNG masks are saved in
+## the same directory of the patch. The function
+## returns the list of the PNG files created
 masks_png_list = createMaskPNGs(tiles_mask_list)
 
+## The PNG images are zipped to be copied to S3
 rgb_zip_file = 'data/bigearthnet_pngs.zip'
 zip_pngs(pngs_list, rgb_zip_file)
 
+## The PNG masks are zipped to be copied to S3
 masks_zip_file = 'data/bigearthnet_mask_pngs.zip'
 zip_pngs(masks_png_list, masks_zip_file)
 
